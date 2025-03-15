32618b341138f28c2f2b709eef3ec2a4a347b4cc
alyson-singleton <55365254+alyson-singleton@users.noreply.github.com>
2025-03-15 09:34:47 -0600

    3mt figures

--- 06_sfig_s2.R
+++ 06_sfig_s2.R
@@ -11,6 +11,7 @@ library(cowplot)
 library(geojsonsf)
 library(cowplot)
 library(sf)
+library(geobr)
 
 #add back in arial font
 library(showtext)
@@ -240,16 +241,20 @@ peru_outline <- st_union(peru$geometry)
 bolivia <- read_sf("~/Desktop/doctorate/ch2 mdd highway/data/shapefiles/bol_adm_gb2014_shp/bol_admbnda_adm1_gov_2020514.shp")
 bolivia <- st_as_sf(bolivia) 
 bolivia$geometry <- st_transform(bolivia$geometry, 4326)
+bolivia_union <- st_union(bolivia)
 bolivia_pando <- bolivia[which(bolivia$ADM1_ES %in% c("Pando")),]
 bolivia_pando <- bolivia_pando[,c(1,11)]
 
 # brazil acre shapefile
 brazil <- st_read("~/Desktop/doctorate/hfi_threshold/brazil_municipality_yearly_hfi_max.csv", geometry_column = ".geo")
+brazil <- st_as_sf(data.frame(brazil, geometry=geojson_sf(brazil$.geo)))
+brazil$geometry <- st_transform(brazil$geometry, 4326)
 brazil_acre <- brazil[brazil$SIGLA_UF=="AC",]
-brazil_acre <- st_as_sf(data.frame(brazil_acre, geometry=geojson_sf(brazil_acre$.geo)))
-brazil_acre$geometry <- st_transform(brazil_acre$geometry, 4326)
 brazil_acre <- st_union(brazil_acre$geometry)
-
+brazil_amazon <- st_read("~/Desktop/doctorate/ch3 amazon network/data/Limites_Amazonia_Legal_2022_shp/Limites_Amazonia_Legal_2022.shp")
+brazil_amazon$geometry <- st_transform(brazil_amazon$geometry, 4326)
+brazil_country <- read_country(year=2020)
+  
 ###################################
 #### load road shapefiles #########
 ###################################
@@ -275,7 +280,12 @@ highway3 <- highway[which(highway$osm_id %in% c(186707203,205371265,225770349,45
                                                 29094033,186768631,186768650,186768629,186768652,186768685,
                                                 29094029,186768691,186768632,186768693,186768628,1035240297,
                                                 29094028,186768672,271201675,271201678,438353973,186768643,
-                                                509105200,221656756,
+                                                509105200,221656756,188513541,462675300,465684097,86493671,
+                                                198924459,82315412,4819058,79095451,4819049,1089136611,
+                                                86493673,198924482,4818990,4819000,792635133,4819825,
+                                                378144776,1089136613,378144772,585246607,378143413,378143412,
+                                                378143416,378143417,378143422,378143419,378143420,378143414,
+                                                378143418,
                                                 233687098,1011499450,1042410885,
                                                 666016966,666016965,1084728138,
                                                 374385515,1042410884,1042410883,
@@ -299,10 +309,10 @@ highway_final$geometry <- st_transform(highway_final$geometry, 4326)
 # brazil roads
 #peru_roads <- st_covers(peru,americas_roads$geometry, sparse = FALSE)
 brazil_norte_roads <- read_sf("~/Desktop/doctorate/ch2 mdd highway/data/shapefiles/norte-latest-free.shp/gis_osm_roads_free_1.shp")
-brazil_norte_roads_primary <- brazil_norte_roads[which(brazil_norte_roads$fclass == 'primary'),]
+#brazil_norte_roads_primary <- brazil_norte_roads[which(brazil_norte_roads$fclass == 'primary'),]
 brazil_norte_roads_primary_estrada <- brazil_norte_roads[which(brazil_norte_roads$name == 'Estrada do Pacífico'),]
-brazil_norte_roads_primary_bool <- st_covers(brazil_acre,brazil_norte_roads_primary_estrada$geometry, sparse = FALSE)
-brazil_norte_roads_primary_estrada <- brazil_norte_roads_primary_estrada[brazil_norte_roads_primary_bool[1,],]
+#brazil_norte_roads_primary_bool <- st_covers(brazil_acre,brazil_norte_roads_primary_estrada$geometry, sparse = FALSE)
+#brazil_norte_roads_primary_estrada <- brazil_norte_roads_primary_estrada[brazil_norte_roads_primary_bool[1,],]
 
 # bolivia roads
 bolivia_roads <- read_sf("~/Desktop/doctorate/ch2 mdd highway/data/shapefiles/bolivia-latest-free.shp/gis_osm_roads_free_1.shp")
@@ -478,54 +488,73 @@ ggsave("SFig2a.pdf", plot=sfig2a, path="~/Desktop/doctorate/ch2 mdd highway/supp
 ############################
 ### 3MT version ############
 ############################
+
 no_axis <- theme(axis.title=element_blank(),
                  axis.text=element_blank(),
                  axis.ticks=element_blank(),
                  panel.grid.major = element_blank())
 
-sfig_3mt <- ggdraw() + 
-  draw_plot(ggplot() +
-              #geom_sf(data = peru_extra_depts, fill='#EEEEEE', color='#a6a6a6', size=.15, show.legend = FALSE) +
-              geom_sf(data = bolivia_pando, fill=NA, color='#a6a6a6', size=.15, show.legend = FALSE) +
-              geom_sf(data = brazil_acre, fill=NA, color='#a6a6a6', size=.15, show.legend = FALSE) +
-              geom_sf(data = mdd_peru, fill='#DDDDDD', color='#a6a6a6', size=.5, show.legend = FALSE) +
-              geom_sf(data = peru_outline, fill=NA, color='black', size=.3, show.legend = FALSE) +
-              geom_sf(data = highway_final, aes(geometry = geometry), color='red', linewidth=0.8, show.legend = "line") +
-              geom_sf(data = brazil_norte_roads_primary_estrada, aes(geometry = geometry), color='red', linewidth=0.8, show.legend = "line") +
-              theme_minimal() +
-              no_axis +
-              theme(legend.text=element_text(size=12),
-                    legend.title=element_text(size=14),
-                    legend.position='none'),
-            0, 0, 1, 1) +
-  draw_plot(ggplot(dengue_df_yearly_summary) +
-              geom_line(aes(x=year, y=incidence)) +
-              geom_vline(xintercept=2008,linetype='dashed', color="red") +
-              #ggtitle("Madre de Dios, Peru") +
-              xlab("Year") + ylab("Dengue\nincidence") + 
-              theme_classic()+
-              xlim(2000,2022)+
-              theme(plot.title = element_text(size=12, face="bold"),
-                    plot.subtitle = element_text(hjust=0.5, size=22),
-                    axis.title=element_text(size=14),
-                    axis.title.y=element_text(size=10, angle=0, vjust=.5, hjust=0.5),
-                    axis.text.y=element_text(size=8),
-                    axis.title.x=element_text(size=10),
-                    axis.text.x=element_text(size=8),
-                    axis.text = element_text(size=10),
-                    legend.text=element_text(size=10),
-                    legend.title=element_text(size=10),
-                    legend.position = "none",
-                    strip.text.x = element_text(size = 10)),
-            0.1, 0.07, 0.22, 0.22)
+sfig_3mt_small_map <- ggplot() +
+  geom_sf(data = bolivia, fill=NA, color='black', size=.15, show.legend = FALSE) +
+  geom_sf(data = mdd_peru, fill='#EEEEEE', color='slategrey', size=.5, show.legend = FALSE) +
+  geom_sf(data = peru_outline, fill=NA, color='black', size=.3, show.legend = FALSE) +
+  geom_sf(data = highway_final, aes(geometry = geometry), color='red', linewidth=0.5, show.legend = "line") +
+  geom_sf(data = brazil_norte_roads_primary_estrada, aes(geometry = geometry), color='red', linewidth=0.5, show.legend = "line") +
+  theme_minimal() +
+  no_axis +
+  theme(legend.text=element_text(size=12),
+        legend.title=element_text(size=14),
+        legend.position='none') +
+  coord_sf(ylim = c(-15, -8),
+           xlim = c(-74,-66),
+           clip = "on",
+           expand = F)
 
-sfig_3mt <- sfig_3mt +                                
-  draw_plot_label(label = c("Peru", "Brazil", "Bolivia"), size = 14, color = "darkred",
-                  x = c(0.33, 0.56, 0.74), y = c(0.6, 0.62, 0.32)) +                                
-  draw_plot_label(label = c("Interoceanic\nHighway"), size = 8, color = "red",
-                  x = c(0.36), y = c(0.37)) 
-sfig_3mt
+sfig_3mt_small_map
+ggsave("sfig_3mt_small_map.pdf", plot=sfig_3mt_small_map, path="~/Desktop/", width = 5, height = 5, units="in", device = "pdf")
 
-ggsave("SFig2a.pdf", plot=sfig_3mt, path="~/Desktop/doctorate/ch2 mdd highway/supplementary_figures/", width = 9.25, height = 8.53, units="in", device = "pdf")
+library(rnaturalearth)
+library(rnaturalearthdata)
+world <- ne_countries(scale = "medium", returnclass = "sf")
+south_america <- world %>% 
+  filter(continent == "South America") %>% 
+  st_union()
+south_america <- st_as_sf(south_america) 
+south_america$geometry <- st_transform(south_america$geometry, 4326)
 
+sfig_3mt_big_map <- ggplot() +
+  geom_sf(data = south_america, fill=NA, color='#5d5a59', size=.15, show.legend = FALSE) +
+  geom_sf(data = bolivia_union, fill=NA, color='black', size=.3, show.legend = FALSE) +
+  geom_sf(data = brazil_country, fill='white', color='black', size=.3, show.legend = FALSE) +
+  geom_sf(data = peru_outline, fill='white', color='black', size=.3, show.legend = FALSE) +
+  theme_minimal() +
+  no_axis +
+  theme(legend.text=element_text(size=12),
+        legend.title=element_text(size=14),
+        legend.position='none')
+
+sfig_3mt_big_map
+ggsave("sfig_3mt_big_map.pdf", plot=sfig_3mt_big_map, path="~/Desktop/", width = 5, height = 5, units="in", device = "pdf")
+
+sfig_3mt_cases_plot <- ggplot(dengue_df_yearly_summary) +
+  geom_line(aes(x=year, y=incidence)) +
+  geom_vline(xintercept=2008,linetype='dashed', color="red") +
+  xlab("") + ylab("") + 
+  theme_classic()+
+  xlim(2000,2022)+
+  theme(plot.title = element_text(size=12, face="bold"),
+        plot.subtitle = element_text(hjust=0.5, size=22),
+        axis.title=element_text(size=14),
+        axis.title.y=element_text(size=10, vjust=.5, angle=0, hjust=0.5),
+        axis.text.y=element_text(size=14),
+        axis.title.x=element_text(size=10),
+        axis.text.x=element_text(size=14),
+        axis.text = element_text(size=10),
+        legend.text=element_text(size=10),
+        legend.title=element_text(size=10),
+        legend.position = "none",
+        strip.text.x = element_text(size = 10))
+
+sfig_3mt_cases_plot
+ggsave("sfig_3mt_cases_plot.pdf", plot=sfig_3mt_cases_plot, path="~/Desktop/", width = 8, height = 4, units="in", device = "pdf")
 
