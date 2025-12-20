fa409bd1a322a9a737d2bac520f09b05f13888d0
alyson-singleton <55365254+alyson-singleton@users.noreply.github.com>
2025-12-19 16:10:11 -0800

    clean up vector dispersal plotting

--- code/34_sfig11_vector_dispersal.R
+++ code/34_sfig11_vector_dispersal.R
@@ -16,6 +16,7 @@ library(ggspatial)
 library(cowplot)
 library(ggrepel)
 library(stringr)
+library(RColorBrewer)
 
 get_legend<-function(myggplot){
   tmp <- ggplot_gtable(ggplot_build(myggplot))
@@ -45,19 +46,10 @@ hfs_lat_long_aedes <- mdd_aedes_aegypti_chronology %>%
 hfs_lat_long_aedes <- hfs_lat_long_aedes %>%
   sf::st_as_sf(coords = c('longitude','latitude'), crs = st_crs(4326))
 
-peru_depts <- read_sf("data/raw/spatial_data/peru_department_shapefiles.shp")
-peru_depts <- st_as_sf(peru_depts) 
-peru_depts$geometry <- st_transform(peru_depts$geometry, 4326)
-peru_outline <- st_union(peru_depts$geometry)
-
 districts <- read_sf("data/raw/spatial_data/mdd_districts.shp")
 districts <- st_as_sf(districts) 
 districts$geometry <- st_transform(districts$geometry, 4326)
 mdd_region <- st_union(districts$geometry)
-
-rivers <- read_sf("data/raw/spatial_data/mdd_rivers.shp")
-rivers <- st_as_sf(rivers, crs=4326) 
-rivers$geometry <- st_transform(rivers$geometry, "EPSG:4326")
 
 roads <- read_sf("data/raw/spatial_data/mdd_roads.shp")
 roads <- st_as_sf(roads) 
@@ -83,108 +75,110 @@ no_axis <- theme(axis.title=element_blank(),
 #####################
 
 highway_mdd$year_paved <- NA_real_
-section_2007_north <- c(94, 33, 96, 17, 32, 25, 43, 44, 26, 7, 45, 46, 13, 99, 100, 9, 42, 103, 104, 11)
-section_2008_north <- c(41, 106, 107, 108, 40, 109, 110, 89, 90, 87, 39, 88, 38, 86)
-section_2009_2010_north_pm <- c(37, 111, 113, 21, 36, 12, 23)
+section_2008_north <- c(94, 95, 33, 96, 17, 32, 25, 43, 44, 26, 7, 45, 46, 13, 99, 100, 101, 102, 8, 9, 10, 42, 103, 104, 105, 11)
+section_2009_north <- c(41, 106, 107, 108, 40, 109, 110, 89, 90, 87, 39, 88, 38, 86)
+section_2010_north <- c(37, 111, 113, 21, 36, 12, 23)
 
-section_2007_middle <- c(34, 24, 30, 28, 19, 35, 19, 6, 5, 80, 81, 49, 4, 48, 58, 57, 76, 3)
-unclear_middle_in_order <- c(91, 22, 118, 64, 65, 119, 120, 121, 122, 123, 124, 56, 125, 126, 127, 128, 130, 83, 131, 84, 134, 135, 55)
+section_2008_middle <- c(34, 24, 30, 28, 19, 35, 19, 6, 5, 80, 81, 49, 4, 48, 58, 57, 76, 3)
 
-section_2008_south <- c(54, 53, 52, 137, 138, 143, 144, 51, 139)
-section_2007_south <- c(140, 141, 142, 97, 98, 50, 68, 2, 77, 73, 74, 72, 69, 70, 71)
+section_2010_south <- c(91, 22, 118, 64, 65, 119, 120, 121, 122, 123, 124, 56, 125, 126, 127, 128, 129, 130, 83, 131, 
+                        132, 133, 84, 82, 134, 135, 136, 55, 112, 114:117, 64, 62, 63, 66, 60, 61)
+section_2009_south <- c(54, 53, 52, 137, 138, 143, 144, 51, 139)
+section_2008_south <- c(140, 141, 142, 97, 98, 50, 68, 1, 2, 77, 73, 74, 72, 69, 70, 71)
 
 highway_mdd <- highway_mdd %>%
   mutate(mvFeatureId = row_number(),
          year_paved = case_when(
-           mvFeatureId %in% section_2007_north            ~ 2007,
-           mvFeatureId %in% section_2008_north            ~ 2008,
-           mvFeatureId %in% section_2009_2010_north_pm    ~ NA_real_,  # or 2010 if you prefer
-           mvFeatureId %in% section_2007_middle           ~ 2007,
-           mvFeatureId %in% unclear_middle_in_order       ~ NA_real_,
-           mvFeatureId %in% section_2008_south            ~ 2008,
-           mvFeatureId %in% section_2007_south            ~ 2007,
-           TRUE                                          ~ NA_real_)) %>%
+           mvFeatureId %in% section_2008_north ~ 2008,
+           mvFeatureId %in% section_2009_north ~ 2009,
+           mvFeatureId %in% section_2010_north ~ 2010,
+           mvFeatureId %in% section_2008_middle ~ 2008,
+           mvFeatureId %in% section_2010_south ~ 2010,
+           mvFeatureId %in% section_2009_south ~ 2009,
+           mvFeatureId %in% section_2008_south ~ 2008,
+           TRUE ~ NA_real_)) %>%
   mutate(year_paved = factor(year_paved))
 
 #####################
-## SFig 11
+## SFig 11a
 #####################
 
-# Build columns for plotting
-center_lat_long_fig1 <- center_lat_long %>%
-  mutate(
-    all_cutoffs = case_when(
-      all_cutoffs %in% c('0', '4', '5', '6', '7') ~ '0',
-      all_cutoffs %in% c('1', '2') ~ '1',
-      TRUE ~ as.character(all_cutoffs))) %>%
-  mutate(
-    all_cutoffs = if_else(!key_connected, '2', all_cutoffs),
-    all_cutoffs = factor(all_cutoffs, levels = c('1', '0', '3', '2')),
-    pm_indicator = if_else(clust == 1, 24, 21),
-    pm_indicator2 = if_else(clust == 1, 3.2, 2.5)) %>%
-  arrange(if_else(clust == 1, 1, 0))
-
-# Coordinates for labeling
-cluster1_coords <- st_coordinates(center_lat_long_fig1 %>% filter(clust == 1))
-arrow_x <- cluster1_coords[1, "X"] + 0.15
-arrow_y <- cluster1_coords[1, "Y"] + 0
+# Coordinates for PM label
+arrow_x <- -69.19735 + 0.15
+arrow_y <- -12.5852 + 0
 label_x <- arrow_x + 0.1
-label_y <- arrow_y - 0.05
+label_y <- arrow_y
 
+# Vector detection formatting
 hfs_lat_long_aedes <- hfs_lat_long_aedes %>%
-  mutate(year = factor(year)) %>%
   filter(!is.na(year))
 
-year_colors <- c(
-  "1999" = "#FDD0C2",
-  "2004" = "#FCAE91",
-  "2005" = "#FB8C6D",
-  "2006" = "#F7684A",
-  "2007" = "#E1412F",
-  "2008" = "#C61B1D",
-  "2009" = "#8A0812"
-)
+hfs_lat_long_aedes_a <- hfs_lat_long_aedes %>%
+  mutate(
+    year_group = case_when(
+      year %in% c(1999, 2004, 2005) ~ "1999–2005 (pre-paving)",
+      TRUE ~ as.character(year)),
+    year_group = factor(
+      year_group,
+      levels = c(
+        "1999–2005 (pre-paving)",
+        "2006", "2007", "2008", "2009")))
 
-# Plot MdD
-mdd_vector_dispersal <- ggplot() +
+# Vector detection colors
+blues <- brewer.pal(9, "Blues")
+year_colors <- c(
+  "1999–2005 (pre-paving)" = blues[2],
+  "2006" = blues[3],
+  "2007" = blues[5],
+  "2008" = blues[7],
+  "2009" = blues[9])
+
+# Crop boundaries
+xlim_crop <- c(-70.6, -68.55)
+ylim_crop <- c(-13.40, -10.8)
+
+sfig11a <- ggplot() +
   geom_sf(data = mdd_region, fill='#ffffff', color='#3b3b3b', size=.15, show.legend = FALSE) +
-  #geom_sf(data = rivers, aes(geometry = geometry, color='Rivers'), linewidth=0.2, show.legend = F) +
-  #geom_sf(data = roads_mdd, aes(geometry = geometry, color='Unpaved Roads'), linewidth=0.5, show.legend = F) +
-  geom_sf(data = highway_mdd, aes(geometry = geometry, color=year_paved), linewidth=2, show.legend = T) +
-  geom_sf(data = hfs_lat_long_aedes, aes(geometry = geometry, fill=year), color='black', shape = 21, size = 5) +
+  geom_sf(data = roads_mdd %>% filter(fclass %in% c("primary", "secondary", "tertiary", "trunk")), 
+          aes(geometry = geometry, color='lightgrey'), linewidth=0.5, alpha=0.4, show.legend = F) +
+  geom_sf(data = highway_mdd %>% filter(!is.na(year_paved)), aes(geometry = geometry, color=year_paved), linewidth=3, show.legend = T) +
+  geom_sf(data = hfs_lat_long_aedes_a, aes(geometry = geometry, fill=year_group), color='black', shape = 21, size = 5) +
+  scale_fill_manual(name = "Year vector detected", values = year_colors) +
+  scale_color_manual(name = "Year highway paved", values = c("2008" = '#F4C2D7', "2009" = '#D86A9E', "2010" = '#9B2F64')) +
   geom_label_repel(
-    data = hfs_lat_long_aedes,
-    stat = "sf_coordinates",            # convert sf → xy for repelling
+    data = hfs_lat_long_aedes_a,
+    stat = "sf_coordinates",
     aes(geometry = geometry, label = year),
-    size = 3,
-    fill = "white",                     # white box behind text
-    label.size = 0.2,                   # border thickness
-    label.padding = unit(0.15, "lines"),
-    box.padding = 0.5,                  # pushes labels farther away
-    point.padding = 0.5,               # padding around each dot
-    label.r = unit(0.05, "lines"),      # small corner rounding
-    segment.color = "black",            # line connecting label and point
+    size = 4,
+    fill = "white",
+    label.size = 0.2,
+    label.padding = unit(0.2, "lines"),
+    box.padding = 0.6,
+    point.padding = 0.6,
+    label.r = unit(0.05, "lines"),
+    segment.color = "black",
     segment.size = 0.5,
     min.segment.length = 0,
     max.overlaps = Inf) +
-  scale_fill_manual(name = "", values = year_colors, na.value = "white") +
-  # scale_fill_manual(name= "", values=c("1" = "#E04490", "0" = "#648FFF", "3" = "grey80", "2" = "#ffffff"),
-  #                   labels=c("Exposed (<5km)", "Unexposed (>10km)", "Buffer (removed)", "Disconnected (removed)")) +
-  #scale_color_manual(name = "", values = c("Unpaved Roads" = '#8c8c8c', "Highway" = 'black',"Rivers" = 'lightblue')) +
   theme_minimal() +
   no_axis +
-  theme(legend.position = "bottom",
-        legend.key.width = unit(0.3, "cm"),
-        legend.key.height = unit(0.3, "cm"),
-        legend.spacing.x = unit(0.02, "cm"),
-        legend.box.spacing = unit(0.01, "cm"),
+  theme(legend.position = "left",
         legend.text = element_text(size = 10),
-        legend.title = element_text(size = 10),
-        legend.direction = "horizontal") +
-  guides(color = guide_legend(nrow = 2),
-         fill = guide_legend(nrow = 2)) +
-  annotation_scale(location = "bl",height = unit(0.1, "cm")) +
-  annotation_north_arrow(location = "br",style = north_arrow_orienteering(text_size = 6), 
+        legend.direction = "vertical") +
+  guides(
+    color = guide_legend(
+      override.aes = list(
+        linewidth = 3,
+        alpha = 1,
+        shape = NA)),
+    fill = guide_legend(
+      override.aes = list(
+        shape = 21,
+        size = 5,
+        color = "black",
+        linewidth = 0))) +
+  annotation_scale(location = "bl", height = unit(0.15, "cm")) +
+  annotation_north_arrow(location = "br",style = north_arrow_orienteering(text_size = 7), 
                          height = unit(0.7, "cm"), width = unit(0.7, "cm")) +
   annotate("segment", 
            x = label_x-0.02, y = label_y, 
@@ -195,10 +189,71 @@ mdd_vector_dispersal <- ggplot() +
            x = label_x, y = label_y, 
            label = "PM", 
            size = 3.4, fontface = "bold", hjust = 0, color = "black") + 
-  geom_point(data = data.frame(lon = -69.19, lat = -12.5933), 
+  geom_point(data = data.frame(lon = -69.185, lat = -12.591), 
              aes(x = lon, y = lat), 
              color = "black", 
-             size = 8, shape = 1, stroke = 1)
-mdd_vector_dispersal
+             size = 12, shape = 1, stroke = 1) +
+  coord_sf(xlim = xlim_crop, ylim = ylim_crop, expand = FALSE)
+sfig11a
 
-ggsave("sfig11.pdf", plot=mdd_vector_dispersal, path="figures/", width = 8, height = 6, units="in", device = "pdf")
+#####################
+## SFig 11b
+#####################
+
+# Add proximate paving year
+hfs_lat_long_aedes$year_paved <- c(NA, NA, NA, 2008, 2008, 2008, 2009, 2009, 2008, 2009, 2010, 2010, NA)
+hfs_lat_long_aedes$year <- as.numeric(as.character(hfs_lat_long_aedes$year))
+
+# Linear fit
+lm_fit <- lm(year ~ year_paved, data = hfs_lat_long_aedes)
+lm_sum <- summary(lm_fit)
+r2_val <- lm_sum$r.squared
+p_val  <- lm_sum$coefficients["year_paved", "Pr(>|t|)"]
+
+set.seed(42)
+sfig11b <- ggplot(hfs_lat_long_aedes, aes(x = year_paved, y = year)) +
+  geom_point(size = 3, color=blues[7], alpha = 0.7,
+             position = position_jitter(width  = 0, height = 0.15)) +
+  geom_smooth(method = "lm", se = TRUE, linewidth = 1, color="#9B2F64", fill  = "#9B2F64", alpha = 0.1) +
+  annotate(
+    "text", x = 2008.2, y = 2009.6,
+    hjust = 0,
+    label = paste0(
+      "R^2 = ", round(r2_val, 3),
+      "\np-val = ", signif(p_val, 2)),
+    size = 4) +
+  labs(x = "Year proximate\nhighway section paved",
+       y = "Year\nvector\ndetected") +
+  scale_x_continuous(
+    breaks = seq(
+      floor(min(hfs_lat_long_aedes$year_paved, na.rm = TRUE)),
+      ceiling(max(hfs_lat_long_aedes$year_paved, na.rm = TRUE)),
+      by = 1)) +
+  scale_y_continuous(breaks = 2005:2011) +
+  coord_cartesian(ylim = c(2005, 2011)) +
+  theme_minimal() +
+  theme(axis.text.x  = element_text(size = 10),
+        axis.text.y  = element_text(size = 10),
+        axis.title.x = element_text(size = 12),
+        axis.title.y = element_text(size = 12, angle = 0, vjust = 0.5))
+sfig11b
+
+#####################
+## SFig 11c
+#####################
+
+
+
+#####################
+## SFig 11all
+#####################
+
+fig11all <- grid.arrange(sfig11a, sfig11b,
+                         ncol = 3, nrow = 2, layout_matrix = rbind(c(1,1,1,2,2),c(1,1,1,NA,NA)))
+
+
+# fig11all <- as_ggplot(fig11all) +                                
+#   draw_plot_label(label = c("A", "B", "C"), size = 14,
+#                   x = c(0.01, 0.48, 0.48), y = c(1, 1, 0.57)) 
+
+ggsave("sfig11.pdf", plot=fig11all, path="figures/", width = 12, height = 7, units="in", device = "pdf") 
